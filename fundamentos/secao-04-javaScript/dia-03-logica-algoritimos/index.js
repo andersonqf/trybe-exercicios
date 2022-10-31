9ef6b14d57692537865fcee4d67a49ecede48089
Anderson <andersonspaju@hotmail.com>
2022-10-31 12:16:23 -0300

    iniciando javaScript Dom e seletores

--- fundamentos/secao-04-javaScript/dia-03-logica-algoritimos/index.js
+++ fundamentos/secao-04-javaScript/dia-03-logica-algoritimos/index.js
@@ -1,6 +1,22 @@
 let items = ['Big Mac', 'Big Tasty', 'Batata', 'Refrigerante', 'Suco'];
 let price = [20, 30, 20, 5, 4];
 
-for ( let index = 0; index < items.length; index += 1) {
-    console.log (items[index] + ' = R$ ' + price[index]);
+// for ( let index = 0; index < items.length; index += 1) {
+//     console.log (items[index] + ' = R$ ' + price[index]);
+// }
+
+//=================================================================================================
+
+let money = 20;
+let cart = []
+
+for (let index = 0; index < items.length; index += 1) {
+  if (price[index] <= money) {
+    cart.push(items[index] + ' = R$ ' + price[index])
+    }
+
+
 }
+
+console.log(cart);
+
