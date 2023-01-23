97b95ebd2380bb3b0d478b28cfcc2959e992f2b2
Anderson de Queiroz Fernandes <andersonspaju@hotmail.com>
2023-01-23 18:15:58 -0300

    tipos primitivos tipagem dinamica e operacoes aritimeticas

--- Fundamentos/secao-03-JavaScript/dia-01-Primeiros_passos/scripts.js
+++ Fundamentos/secao-03-JavaScript/dia-01-Primeiros_passos/scripts.js
@@ -1,10 +1,31 @@
+/* VARIAVEIS */
 const myName = "Anderson";
 const birthCity = "São Paulo";
 let birthYear = 1984;
 birthYear = 2030;
-console.log(myName);
+/* console.log(myName);
 console.log(birthCity);
 console.log(birthYear);
 // Anderson São Paulo 2030 
+ */
 
+/* TIPOS PRIMITIVOS */
+// String
+const myName = "Anderson";
+// Number
+const birthYear = 1984;
+// Boolean
+const isHuman = true;
+// Undefined
+let myJob;
+// Null
+let myCar = null;
+// Object
+const myCar = {
+    brand: "Ford",
+    model: "Fiesta",
+    year: 2019
+};
+//Array
+const myArray = [1, 2, 3, 4, 5];
 
