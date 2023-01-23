c8fe87b18b471be3b74edb85ee1c8873ba86fa86
Anderson de Queiroz Fernandes <andersonspaju@hotmail.com>
2023-01-23 18:09:28 -0300

    aprendendo sobre as variaveis

---
+++ Fundamentos/secao-03-JavaScript/dia-01-Primeiros_passos/scripts.js
@@ -0,0 +1,10 @@
+const myName = "Anderson";
+const birthCity = "São Paulo";
+let birthYear = 1984;
+birthYear = 2030;
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+// Anderson São Paulo 2030 
+
+
