0ae817125044065718ac86c3ed557dff9d2a26f9
Anderson <andersonspaju@hotmail.com>
2023-01-24 14:00:10 -0300

    academia de logica secao 3 dia 2

--- Fundamentos/secao-03-JavaScript/dia-02-arry-e-loopFor/estrutura_de_repeticao.js
+++ Fundamentos/secao-03-JavaScript/dia-02-arry-e-loopFor/estrutura_de_repeticao.js
@@ -1,1 +1,14 @@
-a
+//Estrutura de repeticao (for)
+
+//for (inicio; condicao; incremento) {
+//  codigo a ser executado
+//}
+for (let index =0; index < index.length; index +=1)
+console.log(index);
+
+//for e array
+let nomes = ['Eduardo', 'Maria', 'Joana', 'Wallace', 'Rosana'];
+for (let index = 0; index < nomes.length; index +=1) {
+    let mensagem = "Boas vindas, " + nomes[index] + "!";
+    console.log(mensagem);
+}
