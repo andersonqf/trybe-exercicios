6e26c38a76a4924a6284b278ef4d235f0756f0ec
Anderson de Queiroz Fernandes <andersonspaju@hotmail.com>
2023-01-26 09:33:07 -0300

    adicionado os enunciados dos exercicios da secao 3 dia 3

---
+++ Fundamentos/secao-03-JavaScript/dia-03-Logica-de-Programacao/exercico-pos-aula.js
@@ -0,0 +1,12 @@
+/* Aprofunde seus conhecimentos
+Leia atentamente os enunciados e faça o que se pede.
+
+O fatorial é a multiplicação de um número natural pelos seus antecessores, exceto o zero. Por exemplo:
+Copiar
+O fatorial é representado pelo sinal !
+4! = 4 x 3 x 2 x 1 = 24
+Com base nessas informações, crie um algoritmo que imprima na tela o fatorial de 10.
+
+Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra. Por exemplo, a palavra “banana” seria invertida para “ananab”. Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
+Copiar
+let word = 'tryber'; */
