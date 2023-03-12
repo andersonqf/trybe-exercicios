57bdd28ebdd96842fc28f729d08593c4b07ff5ac
Anderson de Queiroz Fernandes <andersonspaju@hotmail.com>
2023-03-12 17:09:13 -0300

    estudando hofs filter e reduce

---
+++ Fundamentos/secao-07-Hegher_Order_Function_JS/dia-03-filter_reduce/1-Array-filter.js
@@ -0,0 +1,8 @@
+//HOFs
+// Array-filter
+
+// O método filter() cria um novo array com todos os elementos que passaram no teste implementado pela função fornecida.
+
+// Sintaxe
+// arr.filter(callback(element[, index[, array]])[, thisArg])
+
