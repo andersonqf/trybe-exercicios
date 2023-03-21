c68db062fe8f6e8c31678bfd9c000eba3393f952
Anderson de Queiroz Fernandes <andersonspaju@hotmail.com>
2023-03-21 19:17:24 -0300

    iniciando exerc - final do dia

---
+++ 2-Front-end/dia-04-Async-await/b1-Introducao.js
@@ -0,0 +1,3 @@
+// vamos ver sobre async e await
+//async sao funcoes que retornam uma promise e await so pode ser usado dentro de uma funcao async e espera a promise ser resolvida para continuar a execucao do codigo.
+// await é uma palavra reservada do javascript que espera uma promise ser resolvida para continuar a execucao do codigo.
---
+++ 2-Front-end/dia-04-Async-await/b2-Async_Await.js
@@ -0,0 +1,7 @@
+// Como funciona o Async/Await
+
+// 1. Async/Await é uma forma de escrever código assíncrono de forma mais simples e direta.
+// 2. Async/Await é baseado em Promises.
+
+//exemplo 1
+
